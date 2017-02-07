1b418c4db60673f70993a2feb3b87518bf250b7c
calvin <fnastyaptx@gmail.com>
2017-02-07 13:55:36 +0800

    delete unused files

--- library/src/main/java/com/calvin/base/BaseMultiTypeAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.calvin.base;
-
-/**
- * https://github.com/CymChad/BaseRecyclerViewAdapterHelper/blob/master/library/src/main/java/com/chad/library/adapter/base/BaseMultiItemQuickAdapter.java
- * Created by calvin on 2016/8/4 16:31.
- */
-public class BaseMultiTypeAdapter {
-}
